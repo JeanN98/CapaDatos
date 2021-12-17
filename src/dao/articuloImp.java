bfd62425c4f0c22b5b568849f0dd8e022a484716
JeanN98 <jeanpiere1998@gmail.com>
2021-12-17 05:45:18 -0500

    correccion de errores git

--- src/dao/articuloImp.java
+++ src/dao/articuloImp.java
@@ -6,10 +6,9 @@
 package dao;
 
 import Factory.ConnectionDb;
-<<<<<<< HEAD
+
 import Factory.FactoryConexionSqlServer;
-=======
->>>>>>> ed2e87a3394f3f0e1a6c6ae4845bef052da402bd
+
 import Factory.FactoryConnectionDb;
 import model.articulo;
 
@@ -155,7 +154,7 @@ public class articuloImp implements articuloDao {
         try {
             ResultSet rs = this.conn.query(sql.toString());  //carga todos los registros que cumplen con la condicion del sql
 
-<<<<<<< HEAD
+
             while (rs.next()){          //mientras haya registros cargados en el reseltset
               
                 articulo.setID_ARTICULO(rs.getInt("ID_ARTICULO"));
@@ -178,154 +177,5 @@ public class articuloImp implements articuloDao {
         return articulo;             //devuelve el objeto creado
     }
     
-    
-    
-=======
-
-List<articulo> list = new ArrayList<>(); //list es la lista de cuentas bancarias
-
-try{
-ResultSet rs = this.conn.query(sql.toString()); //ejecuta la consulta
-while (rs.next()){ //mientras haya registros en la tabla
-articulo articulo = new articulo();
-articulo.setIdarticulo(rs.getInt("idarticulo"));
-articulo.setCodigo(rs.getInt("codigo"));
-articulo.setDescripcion(rs.getString("descripcion"));
-articulo.setStockactual(rs.getInt("stockactual"));
-articulo.setStockminimo(rs.getInt("stockminimo"));
-articulo.setTipo(rs.getInt("tipo"));
-articulo.setFechacaducidad(rs.getObject("fechacaducidad", LocalDate.class));
-list.add(articulo); //añade el objeto temporal en la lista
-}
-} catch (Exception e) {
-
-} finally {
-this.conn.close(); //cierra la conexion
-}
-return list; //devuelve la lista generada
-}
-
-
-
-@Override
-public articulo edit(int id) {
-this.conn = FactoryConnectionDb.open(FactoryConnectionDb.PGSQL);
-articulo articulo = new articulo();
-
-StringBuilder sql = new StringBuilder(); //para almacenar la consulta e efectuar en la bd
-sql.append("SELECT * FROM \"articulo\" WHERE \"idarticulo\" = ").append(id); //cadena de consulta
-
-try {
-ResultSet rs = this.conn.query(sql.toString()); //carga todos los registros que cumplen con la condicion del sql
-
-
-
-while (rs.next()){ //mientras haya registros cargados en el reseltset
-articulo.setCodigo(rs.getInt("codigo"));
-articulo.setDescripcion(rs.getString("descripcion"));
-articulo.setStockactual(rs.getInt("stockactual"));
-articulo.setStockminimo(rs.getInt("stockminimo"));
-articulo.setTipo(rs.getInt("tipo"));
-articulo.setFechacaducidad(rs.getObject("fechacaducidad", LocalDate.class));
-}
-} catch (Exception e) {
-
-} finally {
-this.conn.close(); //cierra la conexion
-}
-return articulo; //devuelve el objeto creado
-}
-
-
-
-@Override
-public boolean save(articulo articulo) {
-this.conn = FactoryConnectionDb.open(FactoryConnectionDb.SQLSERVER); //abrir la conexion con bd mysql
-boolean save = true; //bandera para indicar si se almacenaron los cambios
-
-try {
-if (articulo.getID_ARTICULO() == 0) { //es cero cuando se esta ingresando un registro nuevo: ver inicializac'on del atributo
-StringBuilder sql = new StringBuilder(); //para crear la sentencia sql
-sql.append("INSERT INTO \"articulo\" (\"codigo\", \"descripcion\", \"stockactual\", \"stockminimo\", \"tipo\", \"fechacaducidad\") VALUES ('").append(articulo.getCodigo());
-sql.append("', '").append(articulo.getDescripcion()); //crear la cadena de conexion
-sql.append("', '").append(articulo.getStockactual()); //crear la cadena de conexion
-sql.append("', '").append(articulo.getStockminimo()); //crear la cadena de conexion
-sql.append("', '").append(articulo.getTipo()); //crear la cadena de conexion
-sql.append("', '").append(articulo.getFechacaducidad()).append("')"); //crear la cadena de conexion
-this.conn.execute(sql.toString()); //ejecuta la query
-} else { //es un registro previamente existente: estamos actualizando
-StringBuilder sql = new StringBuilder(); //para crear la sentencia sql
-
-
-
-sql.append("UPDATE articulo SET codigo = '").append(articulo.getCodigo());
-sql.append("', descripcion = '").append(articulo.getDescripcion()); //crear la cadena de conexion
-sql.append("', stockactual = '").append(articulo.getStockactual()); //crear la cadena de conexion
-sql.append("', stockminimo = '").append(articulo.getStockminimo()); //crear la cadena de conexion
-sql.append("', tipo = '").append(articulo.getTipo()); //crear la cadena de conexion
-sql.append("', fechacaducidad = '").append(articulo.getFechacaducidad()).append("' WHERE idarticulo = ").append(articulo.getIdarticulo()); //crear la cadena de conexion
-
-
-
-/* sql.append("UPDATE \"articulo\" SET \"codigo\" = \'").append(articulo.getCodigo());
-sql.append("\', \"descripcion\" = ").append(articulo.getDescripcion()); //crear la cadena de conexion
-sql.append(", \"stockactual\" = ").append(articulo.getStockactual()); //crear la cadena de conexion
-sql.append(", \"stockminimo\" = ").append(articulo.getStockminimo()); //crear la cadena de conexion
-sql.append(", \"tipo\" = ").append(articulo.getTipo()); //crear la cadena de conexion
-sql.append(", \"fechacaducidad\" = '").append(articulo.getFechacaducidad()).append("' WHERE \"idarticulo\" = ").append(articulo.getIdarticulo()); //crear la cadena de conexion */
-this.conn.execute(sql.toString()); //ejecuta la query
-}
-save = true; //cambia estado de bandera
-} catch(Exception e){
-
-} finally {
-this.conn.close(); //cerrar la conexion
-}
-return save;
-}
-
-
-
-@Override
-public boolean delete(int id) {
-boolean delete = false; //bandera que indica resultado de operacion
-
-
-
-this.conn = FactoryConnectionDb.open(FactoryConnectionDb.PGSQL); //abrir la conexion con bd mysql
-try{
-StringBuilder sql = new StringBuilder(); //para crear la sentencia sql
-sql.append("DELETE FROM \"articulo\" WHERE \"idarticulo\" = ").append(id); //crea la sentencia de borrado
-this.conn.execute(sql.toString()); //ejecuta sentencia sql
-delete = true;
-} catch (Exception e) {
-
-} finally {
-this.conn.close(); //cierra la conexion
-}
-return delete; //devuelve el valor de la bandera
-}
-
-
-
-// @Override
-public boolean actSaldo(int id, float val) {
-this.conn = FactoryConnectionDb.open(FactoryConnectionDb.PGSQL); //abrir la conexion con bd mysql
-boolean save = false; //bandera para indicar si se almacenaron los cambios
-
-try {
-StringBuilder sql = new StringBuilder(); //para crear la sentencia sql
-sql.append("UPDATE \"articulo\" SET \"stockactual\" = \"stockactual\" + ").append(val) .append(" WHERE \"idarticulo\" = ").append(id); //crear la cadena de conexion
-
-
-
-this.conn.execute(sql.toString()); //ejecuta la query
-save = true; //cambia estado de bandera
-} catch(Exception e){
-
-} finally {
-this.conn.close(); //cerrar la conexion
-}
-return save;
->>>>>>> ed2e87a3394f3f0e1a6c6ae4845bef052da402bd
+        
 }
