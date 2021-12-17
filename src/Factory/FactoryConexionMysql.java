03c154b2d6b5866308f8b321ef11d63276c47e93
stalincopa2 <copastalin@gmail.com>
2021-12-16 22:58:32 -0500

    sql serer drier

--- src/Factory/FactoryConexionMysql.java
+++ src/Factory/FactoryConexionMysql.java
@@ -6,28 +6,36 @@ package Factory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import com.mysql.cj.jdbc.Driver;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
  * @author PIERO
  */
-public class FactoryConexionMysql extends ConexionDb{
+public abstract class FactoryConexionMysql extends ConexionDb{
+   
     
         public FactoryConexionMysql(String[] params) {  //recibve un array de string parametros
         this.params = params;    //el atributo params de superclase recibe el parametro del constructor
         this.open();        //llama al metodo que se implementa abajo
     }
-    @Override
-    public Connection open() {   //implementacion del metodo abstracto que abre bd
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/"+this.params[0], this.params[1], this.params[2]);  //subindice 0: nombre bd. paremtro 1: usr; parametro 2: clave.. llega en constructor
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-        return this.connection;     //devuelve la conexion q es atributo declarada en superclase
     
-}
+        public static Connection getConexion(){
+            
+            String conexionUrsl = "jdbc:sqlserver://localhost:1433;"
+              + "database=retinvtl;"
+               +"user=stalin123;"
+               +"password= 12345;"
+               +"loginTimeout=30;"; 
+           try {
+               Connection con = DriverManager.getConnection(conexionUrsl);
+               return con;
+           }catch(SQLException ex){
+               System.out.println(ex.toString());
+               return null;
+           }
+        }
 
 }
