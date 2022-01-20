8fb3b20869955401ea1b21cb901756d4084e6459
JeanN98 <jeanpiere1998@gmail.com>
2022-01-20 18:31:59 -0500

    interfaz inventario

---
+++ src/Inventario/InventarioI.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Inventario;
+
+import model.usuario_cargo;
+
+/**
+ *
+ * @author PIERO
+ */
+public interface InventarioI {
+    
+    ArticulosSucursal(usuario_cargo usuario, int Id_sucursal);
+    
+}
